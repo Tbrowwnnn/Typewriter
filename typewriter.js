831c672cafa07adc661dbe213e4de20ad1d6b21b
Tyler Brown <tyler.brown@telus.com>
2022-11-22 10:36:33 -0700

    updating typewriter

--- typewriter.js
+++ typewriter.js
@@ -1,11 +1,17 @@
-const sentence = "I struggled wtih this exact exercise when I was making a video game as a hobby. This feels like I'm solving an old puzzle that I'd long accepted I couldn't do. \n";
+const sentence = "I struggled wtih this exact exercise when I was making a video game as a hobby. This feels like I'm solving an old puzzle that I'd long accepted I couldn't do.";
 let timer = 1000;
+let finalTime = timer + 50;
+nSentence = sentence + "\n";
 
-for (const char of sentence) {
+for (const char of nSentence) {
   timer += 50;
 
   setTimeout(() => {
     process.stdout.write(char);
   }, timer);
-}
+
+} 
+
+
+
 
